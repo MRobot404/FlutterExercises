754662121cdb3639d801d95556d259027b9ba98b
Oscar Escobar <qomc16@gmail.com>
2024-03-06 22:58:19 -0600

    Ejercicios de dart

--- 05_funciones.dart
+++ 05_funciones.dart
@@ -1,16 +1,14 @@
 void main() {
- print(greetEveryone());
-  print('Suma ${addTwoNumbers(10,20)}');
+  print(greetEveryone());
+  print('Suma ${addTwoNumbers(10, 20)}');
   print('Suma opcional ${addTwoNumbersOptional(10)}');
 }
 
 greetEveryone() => 'Hello everyone!';
 
+int addTwoNumbers(int a, int b) => a + b;
 
-int addTwoNumbers( int a, int b) => a + b;
-
-
-int addTwoNumbersOptional( int a, [int? b]){
-  b = b?? 0;
+int addTwoNumbersOptional(int a, [int? b]) {
+  b = b ?? 0;
   return a + b;
 }
